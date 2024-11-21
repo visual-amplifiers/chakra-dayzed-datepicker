a75b3712594977bb6745bfa0de3d299be66ea3a4
Lewis Coldwell <hello@lewiscoldwell.dev>
2024-11-21 13:40:20 +1100

    allow input wrapper to be controlled

--- src/range.tsx
+++ src/range.tsx
@@ -85,10 +85,10 @@ export const RangeCalendarPanel: React.FC<RangeCalendarPanelProps> = ({
 };
 
 interface RangeProps extends DatepickerProps {
-  selectedDates: Date[];
+  selectedDates: Date[] | undefined;
   configs?: DatepickerConfigs;
   disabled?: boolean;
-  children?: (value: Date[]) => React.ReactNode;
+  children?: (value: Date[] | undefined) => React.ReactNode;
   defaultIsOpen?: boolean;
   closeOnSelect?: boolean;
   onDateChange: (date: Date[]) => void;
@@ -135,7 +135,7 @@ export const RangeDatepicker: React.FC<RangeDatepickerProps> = (props) => {
   } = mergedProps;
 
   // chakra popover utils
-  const [dateInView, setDateInView] = useState(selectedDates[0] || new Date());
+  const [dateInView, setDateInView] = useState(selectedDates && selectedDates[0] || new Date());
   const [offset, setOffset] = useState(0);
   const { onOpen, onClose, isOpen } = useDisclosure({ defaultIsOpen });
 
@@ -153,12 +153,12 @@ export const RangeDatepicker: React.FC<RangeDatepickerProps> = (props) => {
 
   const onPopoverClose = () => {
     onClose();
-    setDateInView(selectedDates[0] || new Date());
+    setDateInView(selectedDates && selectedDates[0] || new Date());
     setOffset(0);
   };
 
   const handleOnDateSelected: OnDateSelected = ({ selectable, date }) => {
-    if (!selectable) {
+    if (!selectable || !selectedDates) {
       return;
     }
     let newDates = [...selectedDates];
@@ -187,10 +187,10 @@ export const RangeDatepicker: React.FC<RangeDatepickerProps> = (props) => {
   };
 
   // eventually we want to allow user to freely type their own input and parse the input
-  let intVal = selectedDates[0]
+  let intVal = selectedDates && selectedDates[0]
     ? `${format(selectedDates[0], datepickerConfigs.dateFormat)}`
     : `${datepickerConfigs.dateFormat}`;
-  intVal += selectedDates[1]
+  intVal += selectedDates && selectedDates[1]
     ? ` - ${format(selectedDates[1], datepickerConfigs.dateFormat)}`
     : ` - ${datepickerConfigs.dateFormat}`;
 
